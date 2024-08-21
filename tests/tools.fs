208f65bf87b10a6723e42abc0b75cc93fe24607b
Patrick Surry <patrick.surry@gmail.com>
2024-08-20 22:00:19 -0400

    fix some tests

--- tests/tools.fs
+++ tests/tools.fs
@@ -146,8 +146,8 @@ T{ blkbuffer 32 capture-output dump restore-output s\" \n
 
 
 : see-/mod-output s\" \n
-nt: *  xt: * \n
-flags: CO 0 IM 0 AN 0 NN 0 HC 0 | UF 1 ST 0 \n
+nt: *  xt: *  header: 04 04 ?? ?? ?? 1A \n
+flags: HC 0 NN 0 AN 0 IM 0 CO 0 DC 1 LC 0 FP 0 | UF 1 ST 0 \n
 size (decimal): 26 \n
 \n
 *  20 ?? ?? A9 FF 48 20 ??  ?? 20 ?? ?? 20 ?? ?? 20   ??..H ? ? ?? ?? \n
@@ -178,8 +178,8 @@ T{ capture-output see /mod restore-output see-/mod-output compare-glob -> 0 }T
 \ CASE has CO+IM+NN flags
 
 : see-case-output s\" \n
-nt: *  xt: * \n
-flags: CO 1 IM 1 AN 0 NN 1 HC 0 | UF 0 ST 0 \n
+nt: *  xt: *  header: 5C 04 ?? ?? ?? 06 \n
+flags: HC 0 NN 1 AN 0 IM 1 CO 1 DC 1 LC 0 FP 0 | UF 0 ST 0 \n
 size (decimal): 6 \n
 \n
 *  CA CA 74 00 74 01                                 ..t.t.\n
@@ -194,8 +194,8 @@ T{ capture-output see case restore-output see-case-output compare-glob -> 0 }T
 \ EXIT has AN flag
 
 : see-exit-output s\" \n
-nt: *  xt: * \n
-flags: CO 1 IM 0 AN 1 NN 0 HC 0 | UF 0 ST 0 \n
+nt: *  xt: *  header: 2C 04 ?? ?? ?? 01 \n
+flags: HC 0 NN 0 AN 1 IM 0 CO 1 DC 1 LC 0 FP 0 | UF 0 ST 0 \n
 size (decimal): 1 \n
 \n
 *  60                                                `\n
@@ -207,8 +207,8 @@ T{ capture-output see exit restore-output see-exit-output compare-glob -> 0 }T
 \ the test variable life (above) has HC flag
 
 : see-life-output s\" \n
-nt: *  xt: * \n
-flags: CO 0 IM 0 AN 0 NN 1 HC 1 | UF 0 ST 0 \n
+nt: *  xt: *   header: C1 04 ?? ?? 05 \n
+flags: HC 1 NN 1 AN 0 IM 0 CO 0 DC 0 LC 0 FP 1 | UF 0 ST 0 \n
 size (decimal): CFA 3  PFA 2 \n
 \n
 *  20 ?? ?? 2A 00   ??*.\n
