d452b45f49942ac8ab7b8e049450b4f0a7716524
Carl Harris <ceharris@vt.edu>
2025-06-24 17:08:47 -0400

    minor cleanup

--- tests/asm.fs
+++ tests/asm.fs
@@ -68,7 +68,7 @@ T{ capture-output see cword restore-output s"  NN 1 " search -rot 2drop -> true 
 \ Testing pseudo-instructions
 T{ here  0a lda.# push-a rts  execute -> 0a }T
 
-\ Testing --> <B for correct displacment
+\ Testing <B for correct displacement
 T{ here 2 lda.# --> dec.a <B bne push-a rts execute -> 0 }T
 
 \ Testing a two-byte instruction for correct operand handling
