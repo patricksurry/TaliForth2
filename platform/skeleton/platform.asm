ee38adba339df6f57566bae56b8aabe970a72fe3
SamCoVT <39203557+SamCoVT@users.noreply.github.com>
2025-06-27 08:57:47 -0400

    Initial files for skeleton platform

--- platform/skeleton/platform.asm
+++ platform/skeleton/platform.asm
@@ -1,16 +1,14 @@
-; Default configuration for c65 (see ../c65/README.md)
+; Skeleton configuration
 ; Scot W. Stevenson <scot.stevenson@gmail.com>
 ; Patrick Surry
 ; Sam Colwell
 ; First version: 19. Jan 2014
-; This version: 27. Feb 2025
+; This version: 26. Jun 2025
 
         ; 65C02 processor (Tali will not compile on older 6502)
         .cpu "65c02"
         ; No special text encoding (eg. ASCII)
         .enc "none"
-
-TALI_ARCH := "c65"
 
 ram_end = $7fff
 
@@ -22,7 +20,33 @@ ram_end = $7fff
 
 TALI_OPTIONAL_WORDS := [ "ed", "editor", "ramdrive", "block", "environment?", "assembler", "disassembler", "wordlist" ]
 
-; define what the CR word should emit to kernel_putc at the end of each line
+; "ed" is a string editor. (~1.5K)
+; "editor" is a block editor. (~0.25K)
+;     The EDITOR-WORDLIST will also be removed.
+; "ramdrive" is for testing block words without a block device. (~0.3K)
+; "block" is the optional BLOCK words. (~1.4K)
+; "environment?" is the ENVIRONMENT? word.  While this is a core word
+;     for ANS-2012, it uses a lot of strings and therefore takes up a lot
+;     of memory. (~0.2K)
+; "assembler" is an assembler. (~3.2K)
+;     The ASSEMBLER-WORDLIST will also be removed if the assembler is removed.
+; "disassembler" is the disassembler word DISASM. (~0.6K)
+;     If both the assembler and dissasembler are removed, the tables
+;     (used for both assembling and disassembling) will be removed
+;     for additional memory savings. (extra ~1.6K)
+; "wordlist" is for the optional SEARCH-ORDER words (eg. wordlists)
+;     Note: Without "wordlist", you will not be able to use any words from
+;     the EDITOR or ASSEMBLER wordlists (they should probably be disabled
+;     by also removing "editor" and "assembler"), and all new words will
+;     be compiled into the FORTH wordlist. (~0.9K)
+
+    
+; TALI_OPTION_CR_EOL sets the character(s) that are printed by the word
+; CR in order to move the cursor to the next line.  The default is "lf"
+; for a line feed character (#10).  "cr" will use a carriage return (#13).
+; Having both will use a carriage return followed by a line feed.  This
+; only affects output.  Either carriage returns or line feeds can be used
+; to terminate lines on the input.
 
 TALI_OPTION_CR_EOL := [ "lf" ]
 ;TALI_OPTION_CR_EOL := [ "cr" ]
@@ -40,7 +64,82 @@ TALI_OPTION_HISTORY := 1
 TALI_OPTION_TERSE := 0
 ;TALI_OPTION_TERSE := 1
 
+; =====================================================================
+; Kernel routines (adapt these to your hardware)
 
+; These are the required kernel routines to interface to
+; our "hardware" which is a simulator in this example configuration.
+; Modify these to suit your hardware.
+
+kernel_init:
+        ; """Initialize the hardware. At the end, we JMP
+        ; to the label `forth` to start the Forth system.
+        ; This will also typically be the target of the reset vector.
+        ; """
+        ; Since the default case for Tali is the py65mon emulator, we
+        ; have no use for interrupts. If you are going to include
+        ; them in your system in any way, you're going to have to
+        ; do it from scratch.
+        sei             ; Disable interrupts
+
+        ; We've successfully set everything up, so print the kernel
+        ; string
+        ldx #0
+-       lda s_kernel_id,x
+        beq _done
+        jsr kernel_putc
+        inx
+        bra -
+_done:
+        jmp forth
+        
+
+kernel_bye:
+        ; """Forth shutdown called from BYE
+        ; If you have a monitor or OS to go back to, put the code
+        ; to do that here."""
+        brk
+
+
+kernel_putc:
+        ; """Print a single character to the console.
+        ;
+        ; Note this routine must preserve X and Y.
+        ; If your code is more complex, wrap it with PHX, PHY ... PLY, PHX
+        ; """
+        sta $F001 ; $F001 is used in many simulators for character output.
+        rts
+
+kernel_getc:
+        ; """Get a single character from the keyboard.
+        ; The c65 io_getc is non-blocking, returning 0 if no key is pressed.
+        ; We convert to a blocking version by waiting for a
+        ; non-zero result.
+        ;
+        ; Note this routine must preserve X and Y but that's easy here.
+        ; If your code is more complex, wrap it with PHX, PHY ... PLY, PHX
+        ; """
+_loop:
+                lda $F004 ; $F004 is used in many simulators for charater input.
+                beq _loop
+                rts
+
+kernel_kbhit:
+        ; """Check if a character is available to be read.
+        ; This should return non-zero when a key is available and 0 otherwise.
+        ; It doesn't consume or return the character itself.
+        ; This routine is only required if you use the KEY? word.
+        ; If you are not going to use the KEY? word, then just return 1.
+        ; """
+                lda #1 ; This always says a key is available
+                rts
+
+
+; =====================================================================
+; Include any forth words written in assembly.  These will be added to
+; the FORTH-WORDLIST.  This must be done BEFORE including taliforth.asm
+; below.
+.include "platform_words.asm"
 
 ; =====================================================================
 ; Include Tali Forth 2 code
@@ -53,106 +152,6 @@ TALI_OPTION_TERSE := 0
 ; In the default configuraiton, we've filled ROM from $8000
 ; to about $dfff, leaving about 8Kb.
 
-; Both py65mon and c65 use $f000-$f010 as their default IO interface
-; and we don't want to change that because it would make it harder to
-; use out of the box, so we just advance past the virtual hardware addresses.
-; (We could also choose to put our kernel routines before the IO addresses.)
-
-io_start = $f000                ; virtual hardware addresses for the simulators
-
-* = io_start
-
-; Define the c65 magic IO addresses relative to $f000
-                .byte ?
-io_putc:        .byte ?         ; $f001     write byte to stdout
-                .byte ?
-io_kbhit:       .byte ?         ; $f003     read non-zero on key ready (c65 only)
-io_getc:        .byte ?         ; $f004     non-blocking read input character (0 if no key)
-                .byte ?
-io_clk_start:   .byte ?         ; $f006     *read* to start cycle counter
-io_clk_stop:    .byte ?         ; $f007     *read* to stop the cycle counter
-io_clk_cycles:  .word ?,?       ; $f008-b   32-bit cycle count in NUXI order
-                .word ?,?
-
-.cerror * != io_start + $10, "Mismatched magic IO interface"
-
-
-; These magic block IO addresses are only implemented by c65 (not py65mon)
-; see c65/README.md for more detail
-
-io_blk_action:  .byte ?         ; $f010     Write to act (status=0 read=1 write=2)
-io_blk_status:  .byte ?         ; $f011     Read action result (OK=0)
-io_blk_number:  .word ?         ; $f012     Little endian block number 0-ffff
-io_blk_buffer:  .word ?         ; $f014     Little endian memory address
-
-.cerror * != io_start + $16, "Mismatched magic block IO interface"
-
-
-; Now we're safe to inject the required kernel routines to interface to
-; our "hardware" which is a simulator in this configuration.
-
-kernel_init:
-        ; """Initialize the hardware. This is called with a JMP and not
-        ; a JSR because we don't have the 6502 stack ready yet. With
-        ; py65mon, of course, this is really easy. At the end, we JMP
-        ; back to the label `forth` to start the Forth system.
-        ; This will also typically be the target of the reset vector.
-        ; """
-                ; Since the default case for Tali is the py65mon emulator, we
-                ; have no use for interrupts. If you are going to include
-                ; them in your system in any way, you're going to have to
-                ; do it from scratch. Sorry.
-                sei             ; Disable interrupts
-
-                ; We've successfully set everything up, so print the kernel
-                ; string
-                ldx #0
--               lda s_kernel_id,x
-                beq _done
-                jsr kernel_putc
-                inx
-                bra -
-_done:
-                jmp forth
-
-kernel_bye:
-        ; """Forth shutdown called from BYE"""
-                brk
-
-kernel_putc:
-        ; """Print a single character to the console.
-        ;
-        ; Note this routine must preserve X, Y but that's easy here.
-        ; If your code is more complex, wrap it with PHX, PHY ... PLY, PHX
-        ; """
-                sta io_putc
-                rts
-
-; c65 and py65mon have different implementations of kernel_getc and kernel_kbhit
-
-
-kernel_getc:
-        ; """Get a single character from the keyboard.
-        ; The c65 io_getc is non-blocking, returning 0 if no key is pressed.
-        ; We convert to a blocking version by waiting for a
-        ; non-zero result.
-        ;
-        ; Note this routine must preserve X and Y but that's easy here.
-        ; If your code is more complex, wrap it with PHX, PHY ... PLY, PHX
-        ; """
-_loop:
-                lda io_getc
-                beq _loop
-                rts
-
-kernel_kbhit:
-        ; """Check if a character is available to be read.
-        ; This should return non-zero when a key is available and 0 otherwise.
-        ; It doesn't consume or return the character itself.
-        ; This routine is only required if you use the KEY? word.
-        ; """
-                lda io_kbhit
-                rts
 
 
 ; Leave the following string as the last entry in the kernel routine so it
---
+++ platform/skeleton/platform_words.asm
@@ -0,0 +1,13 @@
+; platform_words.asm
+; Put any words (written in assembly) that you would like to add for your
+; platform here.  You need to create a header (with the #nt_header macro)
+; and you need to mark the entry with an "xt_wordname:" label and the
+; exit (just before the rts at the end) with a "z_wordname:" label where
+; wordname is replaced with the name of your word.
+
+; You will want to refer to words/headers.asm for a description of how to
+; use the #nt_header macro and the Developer Guide section of the manual.
+
+; Here is an example word that just puts 5 onto the Forth data stack.
+
+#nt_header
